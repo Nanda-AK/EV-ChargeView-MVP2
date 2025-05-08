2e48bcdaaf6068a24a5d88acdf647b796fbae6f0
Nanda-AK <59553626+Nanda-AK@users.noreply.github.com>
2025-05-08 09:40:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,12 @@ st.sidebar.header("Ask a Question or Choose Analysis")
 sample_prompts = [
     "Show a list of vendors along with the total number of charging stations they have installed.",
     "Which electric vehicle charging station vendor has the highest number of stations in the dataset?",
-    "Count the number of EV charging stations in the zip code 95110."
+    "Count the number of EV charging stations in the zip code 95110.",
+    "What is the postal code with the highest count of charging stations in the dataset?",
+    "List all Ev vendors has charging station in postalcode 95112 with there station count",
+    "Which state has the highest number of electric vehicle charging stations based on the provided dataset?", 
+    "List the top 5 states with the highest number of electric vehicle charging stations based on the dataset.", 
+    "Provide a list of the top 5 states with the lowest number of electric vehicle charging stations."
 ]
 
 # --- Single dropdown for sample prompt selection (not nested) ---
